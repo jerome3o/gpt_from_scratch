9f3acad7745151978e334c13561a66a0d5646bd7
Jerome Swannack <jeromeswannack@gmail.com>
2023-02-07 21:53:38 +1300

    * 4

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embed: int):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embed, n_embed),
+            nn.Linear(n_embed, 4 * n_embed),
             nn.ReLU(),
-            nn.Linear(n_embed, n_embed),
+            nn.Linear(4 * n_embed, n_embed),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
