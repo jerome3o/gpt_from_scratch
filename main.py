eb856110db392eeac81476348e6bedbf21974887
Jerome Swannack <jeromeswannack@gmail.com>
2023-02-08 15:35:35 +1300

    Added weights init

--- main.py
+++ main.py
@@ -144,6 +144,17 @@ class Transformer(torch.nn.Module):
         self.ln_f = nn.LayerNorm(N_EMBED)
         self.lm_head = nn.Linear(N_EMBED, vocab_size)
 
+        self.apply(self._init_weights)
+
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     def forward(
         self,
         idx: torch.Tensor,
